3b58ec749819f758e6766f0dc5f6f12904ce5f9f
Seldarzu <arzuuxselda@gmail.com>
2025-07-12 18:36:55 +0300

    Kurulumlar yapıldı

---
+++ teklif-api/src/main/java/com/trafik/teklif_api/TeklifApiApplication.java
@@ -0,0 +1,13 @@
+package com.trafik.teklif_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeklifApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeklifApiApplication.class, args);
+	}
+
+}
---
+++ teklif-api/src/test/java/com/trafik/teklif_api/TeklifApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trafik.teklif_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeklifApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
